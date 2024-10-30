196a25852f4e675e23a0ee6483d6691b6a7805d9
Quinguzzz <quga081028mgtnzra2@cbtis217.edu.mx>
2024-10-30 07:44:59 -0600

    Ingles

---
+++ ingles/ingles/ingles/MainForm.Designer.cs
@@ -0,0 +1,327 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2-PC20
+ * Date: 28/10/2024
+ * Time: 08:11 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ingles
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
+			this.label17 = new System.Windows.Forms.Label();
+			this.label18 = new System.Windows.Forms.Label();
+			this.label19 = new System.Windows.Forms.Label();
+			this.label20 = new System.Windows.Forms.Label();
+			this.label21 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label1.Font = new System.Drawing.Font("Castellar", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(50, 79);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(215, 19);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "English alphabet";
+			// 
+			// label3
+			// 
+			this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+			this.label3.Location = new System.Drawing.Point(-1, -1);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(327, 14);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "         ";
+			// 
+			// label2
+			// 
+			this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label2.Font = new System.Drawing.Font("Castellar", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+			this.label2.Location = new System.Drawing.Point(-1, 8);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(327, 23);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "       AUDIO OF THE THEME";
+			// 
+			// button1
+			// 
+			this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+			this.button1.Font = new System.Drawing.Font("Castellar", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+			this.button1.Location = new System.Drawing.Point(50, 100);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(40, 32);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "🔊";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label4
+			// 
+			this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+			this.label4.Location = new System.Drawing.Point(50, 98);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 10);
+			this.label4.TabIndex = 6;
+			// 
+			// label5
+			// 
+			this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
+			this.label5.Location = new System.Drawing.Point(150, 97);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 10);
+			this.label5.TabIndex = 7;
+			// 
+			// label6
+			// 
+			this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
+			this.label6.Location = new System.Drawing.Point(191, 97);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(74, 26);
+			this.label6.TabIndex = 8;
+			// 
+			// label7
+			// 
+			this.label7.Image = ((System.Drawing.Image)(resources.GetObject("label7.Image")));
+			this.label7.Location = new System.Drawing.Point(95, 106);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(100, 17);
+			this.label7.TabIndex = 9;
+			// 
+			// label8
+			// 
+			this.label8.Image = ((System.Drawing.Image)(resources.GetObject("label8.Image")));
+			this.label8.Location = new System.Drawing.Point(85, 107);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(29, 26);
+			this.label8.TabIndex = 10;
+			// 
+			// label9
+			// 
+			this.label9.Image = ((System.Drawing.Image)(resources.GetObject("label9.Image")));
+			this.label9.Location = new System.Drawing.Point(50, 106);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(10, 27);
+			this.label9.TabIndex = 11;
+			// 
+			// label10
+			// 
+			this.label10.Image = ((System.Drawing.Image)(resources.GetObject("label10.Image")));
+			this.label10.Location = new System.Drawing.Point(50, 123);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(100, 11);
+			this.label10.TabIndex = 12;
+			// 
+			// label11
+			// 
+			this.label11.Image = ((System.Drawing.Image)(resources.GetObject("label11.Image")));
+			this.label11.Location = new System.Drawing.Point(150, 114);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(100, 20);
+			this.label11.TabIndex = 13;
+			// 
+			// label12
+			// 
+			this.label12.Image = ((System.Drawing.Image)(resources.GetObject("label12.Image")));
+			this.label12.Location = new System.Drawing.Point(222, 114);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(39, 20);
+			this.label12.TabIndex = 14;
+			// 
+			// label13
+			// 
+			this.label13.Image = ((System.Drawing.Image)(resources.GetObject("label13.Image")));
+			this.label13.Location = new System.Drawing.Point(244, 79);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(21, 26);
+			this.label13.TabIndex = 15;
+			// 
+			// label14
+			// 
+			this.label14.Image = ((System.Drawing.Image)(resources.GetObject("label14.Image")));
+			this.label14.Location = new System.Drawing.Point(255, 106);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(10, 27);
+			this.label14.TabIndex = 16;
+			// 
+			// label15
+			// 
+			this.label15.Image = ((System.Drawing.Image)(resources.GetObject("label15.Image")));
+			this.label15.Location = new System.Drawing.Point(244, 98);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(21, 15);
+			this.label15.TabIndex = 17;
+			// 
+			// label16
+			// 
+			this.label16.Font = new System.Drawing.Font("Castellar", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label16.Image = ((System.Drawing.Image)(resources.GetObject("label16.Image")));
+			this.label16.Location = new System.Drawing.Point(81, 106);
+			this.label16.Name = "label16";
+			this.label16.Size = new System.Drawing.Size(169, 80);
+			this.label16.TabIndex = 18;
+			this.label16.Text = "A B C D E F G H\nI J K L M N Ñ O\nP Q R S T U V W\nX Y Z";
+			// 
+			// label17
+			// 
+			this.label17.Image = ((System.Drawing.Image)(resources.GetObject("label17.Image")));
+			this.label17.Location = new System.Drawing.Point(247, 133);
+			this.label17.Name = "label17";
+			this.label17.Size = new System.Drawing.Size(18, 27);
+			this.label17.TabIndex = 19;
+			// 
+			// label18
+			// 
+			this.label18.Image = ((System.Drawing.Image)(resources.GetObject("label18.Image")));
+			this.label18.Location = new System.Drawing.Point(243, 160);
+			this.label18.Name = "label18";
+			this.label18.Size = new System.Drawing.Size(22, 26);
+			this.label18.TabIndex = 20;
+			// 
+			// label19
+			// 
+			this.label19.Image = ((System.Drawing.Image)(resources.GetObject("label19.Image")));
+			this.label19.Location = new System.Drawing.Point(50, 160);
+			this.label19.Name = "label19";
+			this.label19.Size = new System.Drawing.Size(31, 26);
+			this.label19.TabIndex = 21;
+			// 
+			// label20
+			// 
+			this.label20.Image = ((System.Drawing.Image)(resources.GetObject("label20.Image")));
+			this.label20.Location = new System.Drawing.Point(50, 133);
+			this.label20.Name = "label20";
+			this.label20.Size = new System.Drawing.Size(31, 27);
+			this.label20.TabIndex = 22;
+			// 
+			// label21
+			// 
+			this.label21.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label21.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label21.ForeColor = System.Drawing.Color.White;
+			this.label21.Image = ((System.Drawing.Image)(resources.GetObject("label21.Image")));
+			this.label21.Location = new System.Drawing.Point(0, 28);
+			this.label21.Name = "label21";
+			this.label21.Size = new System.Drawing.Size(326, 33);
+			this.label21.TabIndex = 23;
+			this.label21.Text = "   ¡Presiona el icono de la bocina para escuchar el audio y                  apre" +
+			"nde a pronunciar el abecedario en ingles!\r\n";
+			// 
+			// button2
+			// 
+			this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+			this.button2.Location = new System.Drawing.Point(50, 194);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(215, 23);
+			this.button2.TabIndex = 24;
+			this.button2.Text = "REGRESAR";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.ClientSize = new System.Drawing.Size(319, 239);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label21);
+			this.Controls.Add(this.label20);
+			this.Controls.Add(this.label19);
+			this.Controls.Add(this.label18);
+			this.Controls.Add(this.label17);
+			this.Controls.Add(this.label16);
+			this.Controls.Add(this.label15);
+			this.Controls.Add(this.label14);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "INGLES";
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label21;
+		private System.Windows.Forms.Label label20;
+		private System.Windows.Forms.Label label19;
+		private System.Windows.Forms.Label label18;
+		private System.Windows.Forms.Label label17;
+		private System.Windows.Forms.Label label16;
+		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
